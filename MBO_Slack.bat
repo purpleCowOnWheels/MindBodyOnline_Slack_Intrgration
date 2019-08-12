d93a25a51a32851d2c91b66d9b6cc2c504b0d662
purpleCowOnWheels <daniel.sky.costanza@gmail.com>
2019-08-11 20:00:15 -0400

    removing bat file

--- MBO_Slack.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\danie\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\danie\Dropbox\Projects\BKR Barre\MBO\MindBodyOnline_Slack_Intrgration\MBO_Slack.py" "M@gicDr@g0n" "Barre32019" "xoxp-588707760694-639365339460-628446373987-2e0cd94589b4478bde8eff2491778bc9"
-pause
